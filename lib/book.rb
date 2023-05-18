b1be953edc0168bd900fd3bcd8aa1a66b84ebea1
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-18 09:58:31 +0300

    added intialize method

--- lib/book.rb
+++ lib/book.rb
@@ -6,6 +6,10 @@ class Book
     attr_accessor :page_count
     attr_accessor :genre
 
+    def initialize(title)
+        @title = title
+    end
+
     def turn_page 
         puts "Flipping the page...wow, you read fast!"
     end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,10 @@ class Shoe
     attr_accessor :material
     attr_accessor :condition
 
+    def initialize(brand)
+        @brand = brand 
+    end
+
     def cobble
         puts "Your shoe is as good as new!"
         @condition = "new"
