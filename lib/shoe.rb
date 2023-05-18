acfcfc28fd7e2ff69f34041c1a5f9de6677507ec
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-18 13:37:56 +0300

    added initialize

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,8 @@ class Shoe
 
     attr_accessor :brand, :color, :size, :material, :condition
 
-    def initialize(brand,color,size,material,condition)
+    def initialize(brand)
         @brand = brand 
-        @color = color
-        @size = size
-        @material = material
-        @condition = condition
     end
 
     def cobble
